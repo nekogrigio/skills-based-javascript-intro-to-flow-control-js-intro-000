eaa96fabcb42dc3407e221d1a38dfc8be9bf4340
Ashley Casey <acasey.dev@gmail.com>
2017-09-01 01:01:16 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -5,8 +5,17 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-
+  if (age >= 13 && age <= 19) {
+    return("You are a teenager!");
+  }
+  else {
+    return("You are not a teenager");
+  }
 }
+
+teenager(13);
+teenager(12);
+teenager(29);
 
 function ageChecker(age) {
 
